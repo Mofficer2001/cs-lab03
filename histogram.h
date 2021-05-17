96783e8d17a8db2465403c25dd1bd56a55d8d996
MagomedovMM <MagomedovMM@mpei.ru>
2021-05-17 17:18:04 +0300

    histogram.h

---
+++ histogram.h
@@ -0,0 +1,8 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+#include <vector>
+
+using namespace std;
+void find_minmax(const vector <double>&  numbers, double& max, double& min);
+
+#endif // HISTOGRAM_H_INCLUDED
