60fcf6055dd9dc0045a4986c3cb74d1ef473e736
MagomedovMM <MagomedovMM@mpei.ru>
2021-05-17 17:17:49 +0300

    histogram.cpp

---
+++ histogram.cpp
@@ -0,0 +1,15 @@
+#include "histogram.h"
+
+void find_minmax(const vector <double>&  numbers, double& max, double& min)
+{
+   // double min,max;
+    max=numbers[0];
+    min=numbers[0];
+    for (double x: numbers)
+    {
+        if (x>max)
+            max=x;
+        else if (x<min)
+            min=x;
+    }
+}
